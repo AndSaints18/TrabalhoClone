bd0ecf99eb512f7118056d27845fadacdbe62d56
Andrei Araripe de Souza e Silva <162312978+AndSaints18@users.noreply.github.com>
2025-05-05 11:48:41 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+//ROTA1
+app.get("/",function(req,res){
+    res.send("Bem vindo ao meu site");
+})
+//ROTA2
+app.get("/produtos",function(req,res){
+    res.send("<h1>Lista de Produtos<h1>");
+})
+//Rota com parametro
+app.get("/consulta/:parametro",function(req,res){
+    res.send("retorno consulta:"+req.params.parametro);
+})
+app.listen(4000,function(erro){
+    if(erro){
+        console.log("Erro ao iniciar.");
+    }else{
+        console.log("Servidor iniciado.");
+    }
+})
