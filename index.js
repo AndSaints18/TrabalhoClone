43500c63493af57676a5696696f5dfbf0215c73f
Andrei Araripe de Souza e Silva <162312978+AndSaints18@users.noreply.github.com>
2025-05-05 18:21:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.get("/produtos",function(req,res){
 app.get("/consulta/:parametro",function(req,res){
     res.send("retorno consulta:"+req.params.parametro);
 })
-app.listen(4000,function(erro){
-    if(erro){
+app.listen(4000, '0.0.0.0', function(erro) {
+    if (erro) {
         console.log("Erro ao iniciar.");
-    }else{
+    } else {
         console.log("Servidor iniciado.");
     }
-})
+});
